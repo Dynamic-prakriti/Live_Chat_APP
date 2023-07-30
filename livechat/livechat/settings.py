540c248987f5795e0f46b84e0ef666b080fe77ec
Prakriti Timilsina <98209027+Dynamic-prakriti@users.noreply.github.com>
2023-07-30 20:07:36 +0545

    Update settings.py

--- livechat/livechat/settings.py
+++ livechat/livechat/settings.py
@@ -19,11 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e5dyn6g8wg5yx7!gr_yh8xqkdc0!kcpr()!+o^z3$7+#!^%jrh'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
